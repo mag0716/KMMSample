8a341fa8124462ab9814450319c51af0f5d4e928
mag0716 <mkishimoto0716@gmail.com>
2022-04-07 22:13:02 +0900

    FakeのApiClientも持たせる

--- kmmapi/src/commonMain/kotlin/com/github/mag0716/kmmsample/kmmapi/ApiClientProvider.kt
+++ kmmapi/src/commonMain/kotlin/com/github/mag0716/kmmsample/kmmapi/ApiClientProvider.kt
@@ -1,5 +1,37 @@
 package com.github.mag0716.kmmsample.kmmapi
 
+import com.github.mag0716.kmmsample.kmmapi.response.Repository
+import io.ktor.client.engine.mock.*
+import io.ktor.http.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
 expect class ApiClientProvider {
     fun provideApiClient(): ApiClient
+}
+
+class FakeApiClientProvider {
+    fun provideApiClient() = ApiClient(
+        httpClientEngine = MockEngine.create {
+            addHandler { request ->
+                val responseHeader =
+                    headersOf("Content-Type" to listOf(ContentType.Application.Json.toString()))
+                with(request.url.fullPath) {
+                    when {
+                        endsWith("/repos") -> {
+                            respond(
+                                content = Json.encodeToString(
+                                    listOf(
+                                        Repository(1L, "name", "fullName")
+                                    )
+                                ),
+                                headers = responseHeader
+                            )
+                        }
+                        else -> error("not support")
+                    }
+                }
+            }
+        }
+    )
 }
