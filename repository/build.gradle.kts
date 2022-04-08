1a35733db8740364963f1db466fd5822a6bcc463
mag0716 <mkishimoto0716@gmail.com>
2022-04-08 22:27:52 +0900

    リポジトリを実装

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -21,7 +21,11 @@ kotlin {
 //    }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":api"))
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
---
+++ repository/src/commonMain/kotlin/com/github/mag0716/kmmsample/repository/GitHubUserRepository.kt
@@ -0,0 +1,12 @@
+package com.github.mag0716.kmmsample.repository
+
+import com.github.mag0716.kmmsample.api.ApiClient
+import com.github.mag0716.kmmsample.api.response.RepositoryResponse
+
+class GitHubUserRepository(
+    private val apiClient: ApiClient
+) {
+    suspend fun getRepositoryList(userName: String): List<RepositoryResponse> {
+        return apiClient.repos(userName)
+    }
+}
